fe43bbb54225880a7f5ff359a09b4504cd3ece02
stellar <stellar@sc.10086.cn.defaultbadlist>
2016-04-09 13:35:31 +0800

    Initial Commit

---
+++ SunShineCarTests/SunShineCarTests.m
@@ -0,0 +1,39 @@
+//
+//  SunShineCarTests.m
+//  SunShineCarTests
+//
+//  Created by stellar on 16/4/9.
+//  Copyright © 2016年 stellar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SunShineCarTests : XCTestCase
+
+@end
+
+@implementation SunShineCarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
