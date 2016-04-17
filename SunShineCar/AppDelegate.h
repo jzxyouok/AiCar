10259869c2c69c352bb3ae032a08621b3c057aca
liuyiyang <870134657@qq.com>
2016-04-17 23:02:48 +0800

    Revert "alkali汽车"
    This reverts commit 1f7b2b9d4b1d32df6872f03c257fccbbed67f6d8.

---
+++ SunShineCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SunShineCar
+//
+//  Created by stellar on 16/4/9.
+//  Copyright © 2016年 stellar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
